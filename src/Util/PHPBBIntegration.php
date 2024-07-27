e2f90affc6fad6f4940f2269ec29789dc48503ab
Scott Wichser <blast007@users.sourceforge.net>
2024-07-27 15:23:25 -0500

    Stop using phpBB's autoloader and only pull in phpBB files when needed.
    This fixes a problem where phpBB's autoloader was including incompatible
    versions of dependencies, such as old versions of psr/container and
    twig.

--- src/Util/PHPBBIntegration.php
+++ src/Util/PHPBBIntegration.php
@@ -32,38 +32,109 @@ class PHPBBIntegration
 {
   protected string $phpbb_database;
   protected string $phpbb_prefix;
-
-  protected $passwords_manager;
-
   protected array $login_config;
 
   public function __construct(Configuration $config, protected PDO $pdo, protected Redis $redis, protected Logger $logger)
   {
-    // TODO: Move most of this to a separate method that is called by methods that require the extra functionality.
-
-    // Expose some variables to the global scope so that phpbb files we include can use them
-    global $phpbb_root_path, $phpEx, $phpbb_container;
-    $phpbb_root_path = $config->get('phpbb.root_path');
+    // Store some configuration information
     $this->phpbb_database = $config->get('phpbb.database');
     $this->phpbb_prefix = $config->get('phpbb.prefix');
     $this->login_config = $config->get('login');
 
-    define('IN_PHPBB', true);
+    // Expose some variables to the global scope so that phpBB files we include can use them
+    global $phpbb_root_path, $phpEx;
+    $phpbb_root_path = $config->get('phpbb.root_path');
     $phpEx = 'php';
-    require($phpbb_root_path . 'includes/startup.' . $phpEx);
-    require($phpbb_root_path . 'phpbb/class_loader.' . $phpEx);
-    $phpbb_class_loader = new \phpbb\class_loader('phpbb\\', "{$phpbb_root_path}phpbb/", $phpEx);
-    $phpbb_class_loader->register();
-    $phpbb_config_php_file = new \phpbb\config_php_file($phpbb_root_path, $phpEx);
-    extract($phpbb_config_php_file->get_all());
-    @define('PHPBB_ENVIRONMENT', 'production');
-    $phpbb_container_builder = new \phpbb\di\container_builder($phpbb_root_path, $phpEx);
-    $phpbb_container = $phpbb_container_builder->with_config($phpbb_config_php_file)->get_container();
-    $phpbb_container->get('request')->enable_super_globals();
-    include($phpbb_root_path.'includes/functions.'.$phpEx);
-    //include($phpbb_root_path.'includes/functions_compatibility.'.$phpEx);
-    $this->passwords_manager = $phpbb_container->get('passwords.manager');
-    include($phpbb_root_path.'includes/utf/utf_tools.'.$phpEx);
+
+    // Tell phpBB files that we're in phpBB, so they don't bail out when we include them
+    define('IN_PHPBB', true);
+  }
+
+  // Wrapper around phpBB's utf8_clean_string function that brings in the needed includes on first use
+  private function utf8_clean_string($string): string
+  {
+    if (!function_exists('utf8_clean_string')) {
+      global $phpbb_root_path, $phpEx;
+
+      // Pull in functions for handling UTF-8 normalization
+      require($phpbb_root_path.'includes/utf/utf_tools.'.$phpEx);
+    }
+
+    return utf8_clean_string($string);
+  }
+
+  // Initialize phpBB's password manager on demand
+  private function get_passwords_manager()
+  {
+    static $passwords_manager;
+
+    if (!$passwords_manager) {
+      global $phpbb_root_path, $phpEx;
+
+      // Load a minimal config so that we can pass something to phpBB classes
+      // Note: In the files we include, only phpbb\passwords\driver::unique_id() uses the config, and that is only called
+      // if get_random_salt() fails to get enough entropy from /dev/urandom
+      require($phpbb_root_path.'phpbb/config/config.'.$phpEx);
+      $phpbb_config = new \phpbb\config\config([
+        'rand_seed' => 0,
+        'rand_seed_last_update' => 0
+      ]);
+
+      // This is used by password drivers
+      require($phpbb_root_path.'phpbb/passwords/driver/driver_interface.'.$phpEx);
+      require($phpbb_root_path.'phpbb/passwords/driver/rehashable_driver_interface.'.$phpEx);
+      require($phpbb_root_path.'phpbb/passwords/driver/base.'.$phpEx);
+      require($phpbb_root_path.'phpbb/passwords/driver/base_native.'.$phpEx);
+
+      // Included because argon2id extends this
+      require($phpbb_root_path.'phpbb/passwords/driver/argon2i.'.$phpEx);
+
+      // This is the currently used modern hash
+      require($phpbb_root_path.'phpbb/passwords/driver/argon2id.'.$phpEx);
+
+      // Included for handling any ancient hashes
+      require($phpbb_root_path.'phpbb/passwords/driver/salted_md5.'.$phpEx);
+
+      // Some extra types that are tried as defaults
+      require($phpbb_root_path.'phpbb/passwords/driver/bcrypt.'.$phpEx);
+      require($phpbb_root_path.'phpbb/passwords/driver/bcrypt_2y.'.$phpEx);
+      require($phpbb_root_path.'phpbb/passwords/driver/phpass.'.$phpEx);
+
+      // Password driver helper stuff
+      require($phpbb_root_path.'phpbb/passwords/driver/helper.'.$phpEx);
+      $phpbb_password_driver_helper = new \phpbb\passwords\driver\helper($phpbb_config);
+
+      // Set up hashing algorithm drivers
+      // TODO: Possibly eliminate some of these if they aren't used/needed (or add others to handle old hashes that need
+      //   to be updated in the database)
+      $hashing_algorithms = [
+        'passwords.driver.argon2i' => new \phpbb\passwords\driver\argon2i($phpbb_config, $phpbb_password_driver_helper),
+        'passwords.driver.argon2id' => new \phpbb\passwords\driver\argon2id($phpbb_config, $phpbb_password_driver_helper),
+        'passwords.driver.bcrypt_2y' => new \phpbb\passwords\driver\bcrypt_2y($phpbb_config, $phpbb_password_driver_helper),
+        'passwords.driver.bcrypt' => new \phpbb\passwords\driver\bcrypt($phpbb_config, $phpbb_password_driver_helper),
+        'passwords.driver.salted_md5' => new \phpbb\passwords\driver\salted_md5($phpbb_config, $phpbb_password_driver_helper),
+        'passwords.driver.phpass' => new \phpbb\passwords\driver\phpass($phpbb_config, $phpbb_password_driver_helper),
+      ];
+
+      // Pull in and create the passwords helper
+      require($phpbb_root_path.'phpbb/passwords/helper.'.$phpEx);
+      $phpbb_password_helper = new \phpbb\passwords\helper();
+
+      // Sets the priority for the default hashing algorithm, with the top being higher priority. The code uses the first
+      // one that is supported.
+      $defaults = [
+        'passwords.driver.argon2id',
+        'passwords.driver.argon2i',
+        'passwords.driver.bcrypt_2y',
+        'passwords.driver.bcrypt',
+        'passwords.driver.salted_md5',
+        'passwords.driver.phpass'
+      ];
+      require($phpbb_root_path.'phpbb/passwords/manager.'.$phpEx);
+      $passwords_manager = new \phpbb\passwords\manager($phpbb_config, $hashing_algorithms, $phpbb_password_helper, $defaults);
+    }
+
+    return $passwords_manager;
   }
 
   public function authenticate_player(string $username, string $password): array
@@ -81,7 +152,7 @@ class PHPBBIntegration
     }
 
     // Clean up UTF-8 characters
-    $username_clean = utf8_clean_string($username);
+    $username_clean = $this->utf8_clean_string($username);
 
     // Try to get user information for this user
     try {
@@ -102,25 +173,29 @@ class PHPBBIntegration
       $key_login_attempts = "CENTRAL:AUTH_ATTEMPTS:{$_SERVER['REMOTE_ADDR']}";
 
       // User exists, valid password
-      if ($this->passwords_manager->check($password, $user['user_password'])) {
+      $passwords_manager = $this->get_passwords_manager();
+      if ($passwords_manager->check($password, $user['user_password'])) {
         // Check if the hash needs to be updated
         // TODO: Test upgrading hashes
-        if ($this->passwords_manager->convert_flag || strlen($user['user_password']) == 32) {
-          $new_hash = $this->passwords_manager->hash($password);
-
-          try {
-            $statement = $this->pdo->prepare("UPDATE {$this->phpbb_database}.{$this->phpbb_prefix}users SET user_password = :user_password WHERE user_id = :user_id");
-            $statement->bindParam('user_password', $new_hash);
-            $statement->bindParam('user_id', $user['user_id'], PDO::PARAM_INT);
-            $statement->execute();
-            // TODO: Check if this works. Some database drivers don't support this.
-            if ($statement->rowCount() != 1) {
-              $this->logger->error('Failed to update password hash algorithm.', ['username' => $username]);
-            } else {
-              $this->logger->info('Successfully upgraded password hash algorithm.', ['username' => $username]);
+        if ($passwords_manager->convert_flag || strlen($user['user_password']) == 32) {
+          $new_hash = $passwords_manager->hash($password);
+          if ($new_hash !== false && strlen($new_hash) > 0) {
+            try {
+              $statement = $this->pdo->prepare("UPDATE {$this->phpbb_database}.{$this->phpbb_prefix}users SET user_password = :user_password WHERE user_id = :user_id");
+              $statement->bindParam('user_password', $new_hash);
+              $statement->bindParam('user_id', $user['user_id'], PDO::PARAM_INT);
+              $statement->execute();
+              // TODO: Check if this works. Some database drivers don't support this.
+              if ($statement->rowCount() != 1) {
+                $this->logger->error('Failed to update password hash algorithm.', ['username' => $username]);
+              } else {
+                $this->logger->info('Successfully upgraded password hash algorithm.', ['username' => $username]);
+              }
+            } catch (PDOException $e) {
+              $this->logger->error('Database error when trying to update password hash algorithm.', ['error' => $e->getMessage(), 'username' => $username]);
             }
-          } catch (PDOException $e) {
-            $this->logger->error('Database error when trying to update password hash algorithm.', ['error' => $e->getMessage(), 'username' => $username]);
+          } else {
+            $this->logger->error('Failed to generated updated password hash algorithm.', ['username' => $username]);
           }
         }
 
@@ -169,7 +244,7 @@ class PHPBBIntegration
   public function get_user_id_by_username(string $username): int|null
   {
     // Clean up UTF-8 characters
-    $username_clean = utf8_clean_string($username);
+    $username_clean = $this->utf8_clean_string($username);
 
     // Try to get user information for this user
     try {
